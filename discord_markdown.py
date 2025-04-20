6464a1f29429e1ba3c452dabee25f4d72579e277
recursivetree <recursivetreemail@gmail.com>
2025-04-20 23:01:01 +0200

    markdown: simplify using tournament parser

--- discord_markdown.py
+++ discord_markdown.py
@@ -1,21 +1,6 @@
 import html
 import re
-
-
-class TextNode:
-    def __init__(self, text: str):
-        self.text: str = text
-
-    def render(self) -> str:
-        return html.escape(self.text)
-
-
-class HtmlNode:
-    def __init__(self, code: str):
-        self.code: str = code
-
-    def render(self) -> str:
-        return self.code
+from typing import Callable
 
 
 class Rule:
@@ -26,52 +11,18 @@ class Rule:
         raise NotImplemented()
 
 
-class CodeBlockRule(Rule):
-    def __init__(self):
-        super().__init__(r"\`\`\`(.+?)\`\`\`")
+class WrapContent(Rule):
+    def __init__(self, regexp: str, formatter: Callable[[str], str], recursive: bool = True):
+        super().__init__(regexp)
+        self.formatter: Callable[[str], str] = formatter
+        self.recursive: bool = recursive
 
     def parse(self, match: re.Match) -> str:
-        content = match.group(1)
-        return f"<pre><code>{html.escape(content)}</code></pre>"
-
-
-class InlineCodeBlockRule(Rule):
-    def __init__(self):
-        super().__init__(r"\`([^`]+?)\`(?!\`)")
-
-    def parse(self, match: re.Match) -> str:
-        content = match.group(1)
-        return f"<code>{html.escape(content)}</code>"
-
-
-class TextStylingRule(Rule):
-    def __init__(self, patter: str, start: str, end: str):
-        super().__init__(patter)
-        self.start: str = start
-        self.end: str = end
-
-    def parse(self, match: re.Match) -> str:
-        content = discord_markdown_to_html(match.group(1))
-        return f"{self.start}{content}{self.end}"
-
-
-class HeaderRule(Rule):
-    def __init__(self, level: int):
-        super().__init__(f"(?<!#){"#" * level}([^\n#]+)")
-        self.level = level
-
-    def parse(self, match: re.Match) -> str:
-        content = discord_markdown_to_html(match.group(1))
-        return f"<h{self.level}>{content}</h{self.level}>"
-
-
-class ListRule(Rule):
-    def __init__(self):
-        super().__init__(r"(?:^|\n)\s*- ([^\n]*?)(?=$|\n)")
-
-    def parse(self, match: re.Match) -> str:
-        content = discord_markdown_to_html(match.group(1))
-        return f"<ul style=\"margin:0\"><li>{content}</li></ul>"
+        if self.recursive:
+            content = discord_markdown_to_html(match.group(1))
+        else:
+            content = html.escape(match.group(1))
+        return self.formatter(content)
 
 
 class UrlRule(Rule):
@@ -84,68 +35,67 @@ class UrlRule(Rule):
 
 
 RULES: list[Rule] = [
-    CodeBlockRule(),
-    InlineCodeBlockRule(),
-    ListRule(),
-    HeaderRule(1),
-    HeaderRule(2),
-    HeaderRule(3),
-    HeaderRule(4),
-    HeaderRule(5),
-    HeaderRule(6),
-    # TODO: rewrite to allow reliable nesting
-    TextStylingRule(r"__\*\*\*([^_\*]+?)\*\*\*__", "<span style=\"text-decoration: underline;\"><b><i>", "</i></b></span>"),
-    TextStylingRule(r"__\*\*([^_\*]+?)\*\*__", "<span style=\"text-decoration: underline;\"><b>", "</b></span>"),
-    TextStylingRule(r"__\*([^_\*]+?)\*__", "<span style=\"text-decoration: underline;\"><i>", "</i></span>"),
-    TextStylingRule(r"(?<!_)_([^_]+?)_(?!_)", "<i>", "</i>"),
-    TextStylingRule(r"~~([^~]+?)~~", "<s>", "</s>"),
-    TextStylingRule(r"(?<!\*)\*([^\*]+?)\*(?!\*)", "<i>", "</i>"),
-    TextStylingRule(r"(?<!\*)\*\*([^\*]+?)\*\*(?!\*)", "<b>", "</b>"),
-    TextStylingRule(r"__([^_]+?)__", "<span style=\"text-decoration: underline;\">", "</span>"),
-    TextStylingRule(r"\*\*\*([^\*]+?)\*\*\*", "<b><i>", "</i></b>"),
+    # structureal elements
+    WrapContent(r"\`\`\`(.+?)\`\`\`", lambda content: f"<pre><code>{content}</code></pre>", recursive=False),  # code blocks
+    WrapContent(r"\`([^`]+?)\`(?!\`)", lambda content: f"<code>{content}</code>", recursive=False),  # inline code
+    WrapContent(r"(?:^|\n)\s*- ([^\n]*?)(?=$|\n)", lambda content: f"<ul style=\"margin:0\"><li>{content}</li></ul>"),  # lists
+    WrapContent(r"#([^\n#]+)", lambda content: f"<h1>{content}</h1>"),  # h1
+    WrapContent(r"##([^\n#]+)", lambda content: f"<h2>{content}</h2>"),  # h2
+    WrapContent(r"###([^\n#]+)", lambda content: f"<h3>{content}</h3>"),  # h3
+    WrapContent(r"####([^\n#]+)", lambda content: f"<h4>{content}</h4>"),  # h4
+    WrapContent(r"#####([^\n#]+)", lambda content: f"<h5>{content}</h5>"),  # h5
+    WrapContent(r"######([^\n#]+)", lambda content: f"<h6>{content}</h6>"),  # h6
+
+    # font styles
+    WrapContent(r"\*\*(.+?)\*\*(?!\*)", lambda content: f"<b>{content}</b>"),  # bold
+    WrapContent(r"\*(.+?)\*", lambda content: f"<i>{content}</i>"),  # italic, important: must be after bold rule so bold isn't interpreted as italic
+    WrapContent(r"__(.+?)__", lambda content: f"<span style=\"text-decoration: underline;\">{content}</span>"),  # underline
+    WrapContent(r"_(.+?)_", lambda content: f"<i>{content}</i>"),  # italic using _, important: must be after underline rule so underline isn't interpreted as italic
+    WrapContent(r"~~(.+?)~~", lambda content: f"<s>{content}</s>"),  # underline
+
     UrlRule(),
 ]
 
 
-def parse_rule(current_ast: list[HtmlNode | TextNode], rule: Rule):
-    new_ast = []
-    for node in current_ast:
-        # if it isn't a text node, we don't have to parse it
-        if not isinstance(node, TextNode):
-            new_ast.append(node)
-            continue
-        # it is a text node, parse it
-        text_start = 0
-        while True:
-            match = rule.pattern.search(node.text, text_start)
-            if match is None:
-                break
-            # finish text node before rule starts
-            text_end = match.start()
-            if text_end - text_start > 0:
-                token_text = node.text[text_start:text_end]
-                new_ast.append(TextNode(token_text))
-            # the rule itself
-            new_ast.append(HtmlNode(rule.parse(match)))
-            # the rest that remains: update start, continue in loop
-            text_start = match.end()
-        # there might be text remaining at the end
-        if len(node.text) - text_start > 0:
-            token_text = node.text[text_start:]
-            new_ast.append(TextNode(token_text))
-    return new_ast
-
-
 def discord_markdown_to_html(markdown: str) -> str:
-    ast = [TextNode(markdown)]
-    for rule in RULES:
-        ast = parse_rule(ast, rule)
-    return "".join((node.render() for node in ast))
+    result = ""
+    node_start = 0
+    while True:
+        best_match: re.Match | None = None
+        best_rule: Rule | None = None
+        best_match_start: int = len(markdown)
+        for rule in RULES:
+            match = rule.pattern.search(markdown, node_start)
+            if match:
+                match_start = match.start()
+                # by using strictly smaller
+                if match_start < best_match_start:
+                    best_match = match
+                    best_rule = rule
+                    best_match_start = match_start
+        if not best_match:
+            # no more special markdown, finish potential text node
+            text_end = len(markdown)
+            if text_end - node_start > 0:
+                token_text = markdown[node_start:text_end]
+                result += html.escape(token_text)
+            break  # we are done
+        else:
+            # we have a match
+            # finish text node until match
+            text_end = best_match.start()
+            if text_end - node_start > 0:
+                token_text = markdown[node_start:text_end]
+                result += html.escape(token_text)
+            # the match itself
+            result += best_rule.parse(best_match)
+            node_start = best_match.end()
+    return result
 
 
 if __name__ == "__main__":
     print(discord_markdown_to_html("""
-    test<> ```\nddd<> ``` ``` `test` 
+    normal ```codeblock``` `inline`
     _italic_ 
     *italic* 
     **bold** 
@@ -170,8 +120,15 @@ if __name__ == "__main__":
     - asd
     - fgh
     
+    This does get escaped: <>
+    
+    ```
+    Styling does not work *inside* code **blocks**
+    ```
+    `Styling does not work *inside* code **blocks**`
+    
     http://localhost/test
     https://example.com
     """))
 
-    print(discord_markdown_to_html(" - good list"))
+    print(discord_markdown_to_html("***bold italics***"))
